4e50d4c8fff484444c680738ab82751dc6f5ed2a
zhoushuyang <812107123@22.com>
2016-03-09 22:15:51 +0800

    first commit pro

---
+++ gitDemo1/gitDemo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDemo1
+//
+//  Created by Qianfeng on 16/3/9.
+//  Copyright © 2016年 ZhouShuYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
